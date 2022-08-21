08f3aafc8388b3ffed3f0f9776a7c576f2f61df4
Ardan <aulia.rahman71@ui.ac.id>
2022-08-21 19:59:51 +0700

    Fixed Get-started button not clicklable

--- js/fancy.js
+++ js/fancy.js
@@ -157,3 +157,24 @@ function endQuiz(correctAnswerData, wrongAnswerData, questionLength) {
     }
 }
 
+function tutorialChanger (btnID){
+    switch (btnID){
+        case 1:
+
+            break
+        case 2:
+
+            break
+        case 3:
+
+            break
+        case 4:
+
+            break
+        case 5:
+
+            break
+        default:
+            break;
+    }
+}
